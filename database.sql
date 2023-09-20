5eddb3245f67924273ef71b50b2112e59c815d43
RisdaUtami <142917563+RisdaUtami@users.noreply.github.com>
2023-09-20 07:45:24 +0700

    Update database.sql
    penambahan database dan perubahan

--- database.sql
+++ database.sql
@@ -6,7 +6,8 @@ use crud_db;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100),
-  `email` varchar(100),
-  `mobile` varchar(15),
+  `kelas` varchar(100),
+  `kontak` varchar(15),
+  `alamat` varchar(100),
   PRIMARY KEY  (`id`)
 );
