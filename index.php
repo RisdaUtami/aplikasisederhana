d7138d6f13a05dd4559422d7d886e67f67a20c8b
RisdaUtami <142917563+RisdaUtami@users.noreply.github.com>
2023-09-20 07:53:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,14 +17,15 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
     <table width='80%' border=1>
  
     <tr>
-        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+        <th>Nama</th> <th>Kelas</th> <th>Kontak</th> <th>Alamat</th> <th>Update</th>
     </tr>
     <?php  
     while($user_data = mysqli_fetch_array($result)) {         
         echo "<tr>";
         echo "<td>".$user_data['name']."</td>";
-        echo "<td>".$user_data['mobile']."</td>";
-        echo "<td>".$user_data['email']."</td>";    
+        echo "<td>".$user_data['kelas']."</td>";
+        echo "<td>".$user_data['kontak']."</td>";
+        echo "<td>".$user_data['alamat']."</td>";   
         echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
     }
     ?>
