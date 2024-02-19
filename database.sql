35f690b2936ba3564c16d88aaa9fa0ff3ccdb832
RisdaUtami <142917563+RisdaUtami@users.noreply.github.com>
2024-02-19 14:28:45 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,4 @@
-/* Create Database and Table */
-create database crud_db;
- 
-use crud_db;
- 
+/* Create Table */
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100),
